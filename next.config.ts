48a7a466705fbb60b8ffb3d43537fcf6578dd87f
Edwin Kim <edwinkim0509@berkeley.edu>
2025-12-15 22:39:26 -0800

    bvbv

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/myportfolio', // Required for GitHub Pages project site
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
